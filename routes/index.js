c45a03a8c73ac2a2f2870d5a89ab613df69845e1
Shivam Piyush <s.piyush46@gmail.com>
2019-08-12 00:25:55 +0530

    removed crawler

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,55 @@
 var express = require('express');
 var router = express.Router();
 const cheerio = require('cheerio');
-var Crawler = require('crawler');
 var scrappingUrl = require('./scrapping-url').scrappingUrl;
-var fs = require('fs')
+var fs = require('fs');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var c = new Crawler({
-    maxConnections: 5,
-    callback: function (error, res, done) {
-      if (error) {
-        console.log(error);
-      } else {
-        $ = cheerio.load(JSON.stringify(res));
-        links = $('a'); //jquery get all hyperlinks
-        var stream = fs.createWriteStream('Web Scrapper.txt');{
-          stream.once('open', function(fd){
-            $(links).each(function (i, link) {
-              if(link && link.attribs && link.attribs.href){
-                var urls = link.attribs.href;
-                console.log(urls);
-                stream.write(urls + '\n');
-              }
-            });
-            stream.end();
-          })
-        }
+  function listDirs(dirsToVisit, maxAtOnce) {
+    let numRunning = 0;
+    let index = 0;
+
+    function runMore() {
+      // while we need to start more, start more of them
+      while (numRunning < maxAtOnce && index < dirsToVisit.length) {
+        ++numRunning;
+        const ls = spawn("ls", [dirsToVisit[index++]]);
+        ls.on("close", code => {
+          --numRunning;
+          console.log(`Finished with code ${code}`);
+          runMore();
+        }).on("error", err => {
+          --numRunning;
+          runMore();
+        });
       }
-      done();
+      if (numRunning === 0) {
+        request(scrappingUrl, (error, response, html) => {
+          if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            links = $('a'); //jquery get all hyperlinks
+            var stream = fs.createWriteStream('Web Scrapper.txt'); {
+              stream.once('open', function (fd) {
+                $(links).each(function (i, link) {
+                  if (link && link.attribs && link.attribs.href) {
+                    var urls = link.attribs.href;
+                    console.log(urls);
+                    stream.write(urls + '\n');
+                  }
+                });
+                stream.end();
+              })
+            }
+          }
+        });
+        // all done with all requests here
+      }
     }
-  });
-  c.queue(scrappingUrl);
+    runMore();
+  }
+  listDirs(5, 5)
   res.send('Done')
 });
 
